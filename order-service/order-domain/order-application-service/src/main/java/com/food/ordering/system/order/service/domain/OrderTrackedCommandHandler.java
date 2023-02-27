eae2c432b9804876f6bb65de1ced17187f263092
weoamathior <weoamathior@gmail.com>
2023-02-27 04:52:21 -0800

    up to lecture 22 - merging in option 1

--- order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/OrderTrackedCommandHandler.java
+++ order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/OrderTrackedCommandHandler.java
@@ -2,14 +2,38 @@ package com.food.ordering.system.order.service.domain;
 
 import com.food.ordering.system.order.service.domain.dto.track.TrackOrderQuery;
 import com.food.ordering.system.order.service.domain.dto.track.TrackOrderResponse;
+import com.food.ordering.system.order.service.domain.entity.Order;
+import com.food.ordering.system.order.service.domain.exception.OrderNotFoundException;
+import com.food.ordering.system.order.service.domain.mapper.OrderDataMapper;
+import com.food.ordering.system.order.service.domain.ports.output.repository.OrderRepository;
+import com.food.ordering.system.order.service.domain.valueobject.TrackingId;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Slf4j
 @Component
 public class OrderTrackedCommandHandler {
+    private final OrderDataMapper orderDataMapper;
+    private final OrderRepository orderRepository;
+
+    public OrderTrackedCommandHandler(OrderDataMapper orderDataMapper, OrderRepository orderRepository) {
+        this.orderDataMapper = orderDataMapper;
+        this.orderRepository = orderRepository;
+    }
+
+    @Transactional(readOnly = true)
     public TrackOrderResponse trackOrder(TrackOrderQuery trackOrderQuery) {
-        log.info("implement me - TODO");
-        return null;
+        Optional<Order> optoinalOrder = orderRepository.findByTrackingId(new TrackingId(trackOrderQuery.getOrderTrackingId()));
+
+        if (optoinalOrder.isEmpty()) {
+            String message = String.format("Could not find order with tracking id %s", trackOrderQuery.getOrderTrackingId());
+            log.warn(message);
+            throw new OrderNotFoundException(message);
+        }
+
+        return orderDataMapper.orderToTrackOrderResponse(optoinalOrder.get());
     }
 }
